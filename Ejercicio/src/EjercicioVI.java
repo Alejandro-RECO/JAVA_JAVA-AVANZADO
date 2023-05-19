6d89816645401e3b38b736ee822617833fecc33c
nobrobert <githubbarreto@gmail.com>
2023-05-19 07:56:06 -0500

    organizing repository structure
    organizing repository structure

--- Ejercicio/src/EjercicioVI.java
+++ Ejercicio/src/EjercicioVI.java
@@ -1,36 +1,61 @@
-// importamos el objeto de Scaner de Java
 import java.util.Scanner;
-
 public class EjercicioVI {
     public static void main(String[] args) {
-
-    // Declaramos las variables para nuestro sistema
-
+        // Declaramos las variables del sistema
+        int num1, num2, suma, resta, multiplicacion, divicion, operado;
         String respuesta;
+        // Solicitamos la informacion al usuario y la almacenamos, instanciamos el objeto scaner de javax 
+        
         Scanner leer = new Scanner(System.in);
-    
-    // Presentamos nuestro programa y solicitamos la información
-
-        System.out.println("¡Hola! queremos saber cual es tu color favarito para determinar cual es tu fruta segun tu eleccion");
-        System.out.println("¿Nos podrias decir cual es tu color favorito?");
+        System.out.println("¿Que operación deseas realizar? \n Suma \n Resta \n Multiplicacion \n Divición \n Potenciación");
         respuesta = leer.nextLine();
-
-    // Ejecutamos las condiciones de nuestro sistema para determinar el tipo de fruta asignada   
-     
-        if(respuesta.equalsIgnoreCase("verde")){
-            System.out.println("Grandioso al parecer elegiste " + respuesta + " la fruta asignada para ti es la Manzana");
-        }else if (respuesta.equalsIgnoreCase("rojo")){
-            System.out.println("Grandioso al parecer elegiste " + respuesta + " la fruta asignada para ti es la Fresa");
-        }else if (respuesta.equalsIgnoreCase("morado")){
-            System.out.println("Grandioso al parecer elegiste " + respuesta + " la fruta asignada para ti es la Mora");
-        }else if (respuesta.equalsIgnoreCase("amarillo")){
-            System.out.println("Grandioso al parecer elegiste " + respuesta + " la fruta asignada para ti es el Banano");
+        // Hacemos un condicinal para aclarar que solo se utilicen las operaciones del sistema
+        if(respuesta.equalsIgnoreCase("suma")||
+            respuesta.equalsIgnoreCase("resta")||
+            respuesta.equalsIgnoreCase("multiplicacion")||
+            respuesta.equalsIgnoreCase("divicion")||
+            respuesta.equalsIgnoreCase("potenciacion")){
+        // Ejecutamos el sistema partiendo de la respuesta, pedimos los numeros a calcular
+                System.out.println("Digite los numeros a realizar el calculo: \nprimer numero: ");
+                num1 = leer.nextInt();
+                System.out.println("Segundo numero: ");
+                num2 = leer.nextInt();
+        // Ejecutamos la operacion elegida, y mostramos el resultado
+                switch(respuesta.toLowerCase()){
+                    case "suma":
+                    suma = num1 + num2;
+                    operado = suma;
+                    System.out.println("Resultado de "+num1+ " + "+ num2 + " es: "+operado+ ".");
+                    break;
+                    case "resta":
+                    resta = num1 - num2;
+                    operado = resta;
+                    System.out.println("Resultado de "+num1+ " - "+ num2 + " es: "+operado+ ".");
+                    break;
+                    case "multiplicacion":
+                    multiplicacion = num1 * num2;
+                    operado = multiplicacion;
+                    System.out.println("Resultado de "+num1+ " * "+ num2 + " es: "+operado+ ".");
+                    break;
+                    case "divicion":
+                    divicion = num1 / num2;
+                    operado = divicion;
+                    System.out.println("Resultado de "+num1+ " / "+ num2 + " es: "+operado+ ".");
+                    break;
+                    case "potenciacion":
+                    System.out.println("Resultado de "+num1+ " potneciado "+ num2 + " es: "+ Math.pow(num1, num2)+ ".");
+                    break;
+            }
+        // Mostramos alerta si no se elige ninguna opción ni se digita algun parametro
         }else if (respuesta.equals("")){
-            System.out.println("Ups! al parecer no respondiste nada, y así no podre asignarte una fruta");
-        }else{
-            System.out.println("Ups! al parecer no tengo una respuesta para " + respuesta + " lo lamento mucho :(");
+            System.out.println("No elegiste ninguna operación intenta de nuevo.");
         }
-    
+        // Mostramos alerta por operacion o dato no aclarado
+        else{
+              System.out.println("No se reconoce la operacion a realizar.");
+        }
+        System.out.println("Gracias por utilizar el sistema :)");
+        // Finalizamos el beff
         leer.close();
     }
 }
