e6864485b1676fa0bcef4ce234e9228e1563a29b
Alejandro-RECO <revolledocoronado@hotmail.com>
2023-05-28 21:18:28 -0500

    commenting the code
    We comment on the operation of the code

--- Ejercicio/src/EjercicioXIII.java
+++ Ejercicio/src/EjercicioXIII.java
@@ -2,19 +2,20 @@ import java.util.Scanner;
 
 public class EjercicioXIII {
     public static void main(String[] args) {
+        // Creamos una matris con 2 filas y cuatro columnas que almacena los pares de palabras 
         String[][] matriz = {
                 {"gato", "rana", "mariposa", "perro"},
                 {"mariposa", "perro", "rana", "gato"}
         };
 
         Scanner scanner = new Scanner(System.in);
-
+        // Definimos el numero de oportunidades y inicializamos el contador de puntos
         int oportunidades = 5;
         int paresAdivinados = 0;
-
+        // Creamos un ciclo wile que termina cunado las oportunidades lleguen a 0 o que se adivinen los cuatro pares de la matriz 
         while (oportunidades > 0 && paresAdivinados < 4) {
             System.out.println("Oportunidades restantes: " + oportunidades);
-
+            // Solicitamos las posiciones al usuario, y verificamos que no sea un valor incorrecto 
             System.out.print("Ingrese la fila (0-1): ");
             int fila1 = scanner.nextInt();
             System.out.print("Ingrese la columna (0-3): ");
@@ -34,13 +35,13 @@ public class EjercicioXIII {
                 System.out.println("Posición inválida. Inténtalo de nuevo.");
                 continue;
             }
-
+            // Almacenamos las posiciones del usuario dentro de la matriz (es decir la posicion que ocupa dentro de la matriz)
             String palabra1 = matriz[fila1][columna1];
             String palabra2 = matriz[fila2][columna2];
-
+            // Mostramos las palabras que corresponden a las cordenas escritas por el usuario 
             System.out.println("Palabra seleccionada en la posición [" + fila1 + ", " + columna1 + "]: " + palabra1);
             System.out.println("Palabra seleccionada en la posición [" + fila2 + ", " + columna2 + "]: " + palabra2);
-
+            // Comparamos que el las palabras correspondientes a las cordenasdas sean iguales o no 
             if (palabra1.equals(palabra2)) {
                 System.out.println("¡Correcto! Las palabras coinciden.");
                 paresAdivinados++;
@@ -51,7 +52,7 @@ public class EjercicioXIII {
 
             System.out.println();
         }
-
+        // Mensaje de finalizacion del sistema partiendo de la condicion cumplida
         if (paresAdivinados == 4) {
             System.out.println("¡Felicidades! Has encontrado todos los pares. Ganaste el juego.");
         } else {
